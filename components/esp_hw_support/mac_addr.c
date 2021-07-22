50283ade4cf105f4e576b2d52be7039bbf9e04b2
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-07-22 18:20:40 +0200

    Serial.printf (esp_efuse_get_pkg_ver());

--- components/esp_hw_support/mac_addr.c
+++ components/esp_hw_support/mac_addr.c
@@ -107,14 +107,14 @@ esp_err_t esp_efuse_mac_get_default(uint8_t* mac)
         if (((mac_high & 0xFFFF) == 0x18fe) && (mac_low >= 0x346a85c7) && (mac_low <= 0x346a85f8)) {
             return ESP_OK;
         } else {
-            uint32_t chip_ver = REG_GET_FIELD(EFUSE_BLK0_RDATA3_REG, EFUSE_RD_CHIP_VER_PKG);
-            uint32_t pkg_version = chip_ver & 0x7;
-            if (3 == pkg_version) {
+            Serial.printf (esp_efuse_get_pkg_ver());
+            if (esp_efuse_get_pkg_ver() ==  3) {
             ESP_LOGI(TAG, "Found OEM-type ESP32, ignore EFUSE CRC error ...");
             return ESP_OK; // override for Xiaomi SOC's and maybe others too
             }
             ESP_LOGE(TAG, "Base MAC address from BLK0 of EFUSE CRC error, efuse_crc = 0x%02x; calc_crc = 0x%02x", efuse_crc, calc_crc);
-            abort();
+            return ESP_OK;
+            // abort();
         }
     }
 #endif // CONFIG_IDF_TARGET_ESP32
