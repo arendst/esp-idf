99c2b487f19010ed65bd2213c81e60bf56b2169c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-07-22 19:28:20 +0200

    Update mac_addr.c

--- components/esp_hw_support/mac_addr.c
+++ components/esp_hw_support/mac_addr.c
@@ -107,7 +107,7 @@ esp_err_t esp_efuse_mac_get_default(uint8_t* mac)
         if (((mac_high & 0xFFFF) == 0x18fe) && (mac_low >= 0x346a85c7) && (mac_low <= 0x346a85f8)) {
             return ESP_OK;
         } else {
-            Serial.printf (esp_efuse_get_pkg_ver());
+            printf (esp_efuse_get_pkg_ver());
             if (esp_efuse_get_pkg_ver() ==  3) {
             ESP_LOGI(TAG, "Found OEM-type ESP32, ignore EFUSE CRC error ...");
             return ESP_OK; // override for Xiaomi SOC's and maybe others too
